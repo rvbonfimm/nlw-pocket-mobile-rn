d5891de6d76babecc49422e02a9458b6aaf366a4
rvbonfimm <rogeriovilelabonfim@gmail.com>
2024-12-12 15:43:30 -0300

    feat: add _layout router config

---
+++ src/app/_layout.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "expo-router"
+import { 
+    useFonts,
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold
+ } from "@expo-google-fonts/rubik"
+
+import { colors } from "@/src/styles/theme"
+import Loading from "./components/loading"
+
+export default function Layout() {
+    const [fontsLoaded] = useFonts({
+        Rubik_600SemiBold,
+        Rubik_400Regular,
+        Rubik_500Medium,
+        Rubik_700Bold
+    })
+
+    if (!fontsLoaded) {
+        return <Loading />
+    }
+    return <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: colors.gray[100] } }}></Stack>
+}
