9d872ce22e3d9aaff0163026a7574beca48234ee
rvbonfimm <rogeriovilelabonfim@gmail.com>
2024-12-12 22:16:24 -0300

    feat: add step and steps component

---
+++ src/app/components/step/index.tsx
@@ -0,0 +1,22 @@
+import { View, Text } from 'react-native'
+import { IconProps } from "@tabler/icons-react-native"
+
+import { s } from './styles'
+import { colors } from "@/src/styles/theme"
+
+type Props = {
+    title: string
+    description: string
+    icon: React.ComponentType<IconProps>
+}
+export function Step({ title, description, icon: Icon }: Props) {
+    return (
+        <View style={s.container}>
+            { Icon && <Icon size={32} color={colors.red.base}></Icon> }
+            <View style={s.details}>
+                <Text style={s.title}>{title}</Text>
+                <Text style={s.description}>{description}</Text>
+            </View>
+        </View>
+    )
+}
